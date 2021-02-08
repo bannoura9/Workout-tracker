81be4616313232a6566fd5ba6879eacf47c86a25
Mike Bannoura <mikebannoura@gmail.com>
2021-02-08 01:15:38 -0700

    chore: update db uri

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -9,16 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+/* console.log("process.env.MONGODB_URI: ", process.env.MONGODB_URI);
+console.log("process.env.MONGODB_URI: ", process.env); */
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workouts", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/routeshtml.js"));
 app.use(require("./routes/routesapi.js"));
-
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
